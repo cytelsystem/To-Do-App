9549f805c1c9a00a1bf9d2d3ca827fe1a975c0d1
Hector Javier Moreno <cytelsystem@gmail.com>
2022-04-13 17:14:07 -0500

    fsdfsdfa

--- scripts/login.js
+++ scripts/login.js
@@ -10,8 +10,14 @@ window.addEventListener('load', function() {
     /* -------------------------------------------------------------------------- */
     form.addEventListener('submit', function(event) {
 
+        event.preventDefault()
 
+        let dataLogin = {
+            email: document.querySelector('#inputEmail').value,
+            password: document.querySelector('#inputPassword').value,
+          }
 
+          realizarLogin(dataLogin)
 
 
     });
@@ -20,7 +26,42 @@ window.addEventListener('load', function() {
     /* -------------------------------------------------------------------------- */
     /*                     FUNCIÓN 2: Realizar el login [POST]                    */
     /* -------------------------------------------------------------------------- */
-    function realizarLogin(settings) {
+    function realizarLogin(dataLogin) {
+
+        let settings = {
+            "method": "POST",
+            "headers": {
+              "content-type": "application/json"
+            },
+            "body": JSON.stringify(dataLogin)
+          }
+
+          Url = "https://ctd-todo-api.herokuapp.com/v1/users/login"
+          fetch(Url, settings)
+
+          .then(function(respuesta){
+              
+            return respuesta.json()
+
+          })
+
+          .then(function(informacion){
+            console.log("informacion que retorna" + " " + informacion);
+
+            if (informacion == "El usuario ya se encuentra registrado") {
+                alert("El usuario ya se encuentra registrado")
+
+            } else {
+                localStorage.setItem(data.email, JSON.stringify(informacion));
+                console.table(localStorage)
+            }
+
+
+          })
+          .catch(function(e){
+            console.log("Error! " + e)
+           })
+
 
 
 
